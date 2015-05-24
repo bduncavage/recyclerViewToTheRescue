f20941fd11ca0a3c017149db7684504537524b4b
Brett Duncavage <brett.duncavage@rd.io>
2015-05-24 11:00:48 -0700

    Add tab strip to view pager
    Also updated some colors.

--- RecyclerViewDemo/app/src/main/java/org/duncavage/recyclerviewdemo/fragments/ListPagerFragment.java
+++ RecyclerViewDemo/app/src/main/java/org/duncavage/recyclerviewdemo/fragments/ListPagerFragment.java
@@ -5,7 +5,9 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,12 +19,20 @@ import org.duncavage.recyclerviewdemo.R;
  */
 public class ListPagerFragment extends Fragment {
     private ViewPager viewPager;
+    private PagerTabStrip tabStrip;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_viewpager, container, false);
+
         viewPager = (ViewPager)rootView.findViewById(R.id.view_pager);
+        tabStrip = (PagerTabStrip)rootView.findViewById(R.id.tab_strip);
+        tabStrip.setBackgroundColor(getResources().getColor(R.color.toolbar_background));
+        tabStrip.setTextColor(getResources().getColor(R.color.dark_text));
+        tabStrip.setTextSize(TypedValue.COMPLEX_UNIT_PX, getResources().getDimensionPixelSize(R.dimen.textSizeTC3));
+        tabStrip.setTabIndicatorColor(getResources().getColor(R.color.rdio_blue));
+
         return rootView;
     }
 
@@ -32,9 +42,14 @@ public class ListPagerFragment extends Fragment {
         viewPager.setAdapter(new ListPagerAdapter(getChildFragmentManager()));
     }
 
-    private static class ListPagerAdapter extends FragmentPagerAdapter {
+    private class ListPagerAdapter extends FragmentPagerAdapter {
         public ListPagerAdapter(FragmentManager fm) {
             super(fm);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return getTitleForLayoutType(RecyclerViewFragment.LayoutType.values()[position]);
         }
 
         @Override
@@ -45,6 +60,19 @@ public class ListPagerFragment extends Fragment {
         @Override
         public int getCount() {
             return RecyclerViewFragment.LayoutType.values().length;
+        }
+
+        private String getTitleForLayoutType(RecyclerViewFragment.LayoutType type) {
+            switch (type) {
+                case Linear:
+                    return getString(R.string.page_title_linear);
+                case Grid:
+                    return getString(R.string.page_title_grid);
+                case GridWithSpans:
+                    return getString(R.string.page_title_grid_with_spans);
+                default:
+                    return null;
+            }
         }
     }
 }
