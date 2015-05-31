30bc77ecc30390d04c3a50e158f2bda18d7a5535
Brett Duncavage <brett.duncavage@rd.io>
2015-05-30 21:34:01 -0700

    Updated how spannable items are added
    Don't put spannable items adjacent in the list.

--- RecyclerViewDemo/app/src/main/java/org/duncavage/recyclerviewdemo/presenters/GridWithHeadingsAndSpansDemoDataListPresenter.java
+++ RecyclerViewDemo/app/src/main/java/org/duncavage/recyclerviewdemo/presenters/GridWithHeadingsAndSpansDemoDataListPresenter.java
@@ -4,10 +4,15 @@ import org.duncavage.recyclerviewdemo.R;
 import org.duncavage.recyclerviewdemo.presenters.views.ListView;
 import org.duncavage.recyclerviewdemo.viewmodels.ListItemViewModel;
 
+import java.util.Random;
+
 /**
  * Created by brett on 5/25/15.
  */
 public class GridWithHeadingsAndSpansDemoDataListPresenter extends GridWithHeadingsDemoDataListPresenter {
+    private static final int NO_POSITION = -1;
+    private int lastSpannableItemAddedPosition = NO_POSITION;
+
     public GridWithHeadingsAndSpansDemoDataListPresenter(ListView<ListItemViewModel> view, int spanCount) {
         super(view, spanCount);
     }
@@ -15,9 +20,16 @@ public class GridWithHeadingsAndSpansDemoDataListPresenter extends GridWithHeadi
     @Override
     protected void onItemAdded(ListItemViewModel viewModel, int position) {
         super.onItemAdded(viewModel, position);
-        if (position > 0 && position % 5 == 0 || position % 6 == 0) {
-            viewModel.spanCount = getSpanCount() - 1;
-            viewModel.layout = R.layout.grid_item_large;
+        if (position > 0) {
+            int rand = new Random().nextInt() % 3;
+            // 33% chance of adding a span.
+            // Don't put spannable items next to each other, it doesn't look good.
+            if ((lastSpannableItemAddedPosition == NO_POSITION
+                    || lastSpannableItemAddedPosition != position - 1) && rand == 2) {
+                viewModel.spanCount = getSpanCount() - 1;
+                viewModel.layout = R.layout.grid_item_large;
+                lastSpannableItemAddedPosition = position;
+            }
         }
         // TODO: Add a full-bleed item that is a horizontal list as the last item of the group
         // Check if next position is a header or not
